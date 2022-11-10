f4a1d9a01924e2b0807ebb06985fdf97e63e5d74
ViktorZvonar <viktorzvonar@yahoo.com>
2022-11-10 17:28:04 +0200

    two-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+console.log(ulEl);
+
+const createGallaryMarkup = (galleryItems) => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}"
+       
+        />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+};
+
+const gallaryMarkup = createGallaryMarkup(galleryItems);
+ulEl.insertAdjacentHTML("beforeend", gallaryMarkup);
+
+const onDivElClick = (event) => {
+  event.preventDefault();
+  const isPicture = event.target.classList.contains("gallery__image");
+
+  if (!isPicture) {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+};
+
+ulEl.addEventListener("click", onDivElClick);
